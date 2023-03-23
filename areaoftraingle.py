36a96440fbaae768c24ba548810015009fef5219
Sahil <leviack629@gmail.com>
2023-03-24 00:14:57 +0530

    Cel to Fehran

---
+++ areaoftraingle.py
@@ -0,0 +1,7 @@
+a = float(input('Enter first side: '))
+b = float(input('Enter second side: '))
+c = float(input('Enter third side: '))
+
+s = (a+b+c) / 2
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print(f'Area of the triangle with sides {a}, {b} and {c} is {area} unit sq.')
---
+++ celtoFer.py
@@ -0,0 +1,3 @@
+temp_in_celsius = float(input("Enter the temp in celsius: "))
+temp_in_fehrenheit = temp_in_celsius * 1.8+ 32
+print(f'The temp in Fahrenheit is {temp_in_fehrenheit}')
